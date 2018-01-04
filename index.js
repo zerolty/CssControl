91414636af8549cec2751674cfb06b221b65b289
hua1995116 <461249104@qq.com>
2018-01-04 17:56:02 +0800

    'init'

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+router.get('/', function (req, res, next) {
+  req.url = './index.html';
+  next();
+});
+app.use(router);
+
+app.use(express.static('./dist'));
+const server = app.listen(3000)
