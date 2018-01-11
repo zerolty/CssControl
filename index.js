69eb93f2d7018a8f2ba497fe0e09f4b901a43587
hua1995116 <461249104@qq.com>
2018-01-11 11:49:30 +0800

    '完成多页的遍历'

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const path = require('path');
-const argv = require('optimist').argv;
+const argv = require('optimist')
+        .alias('p', 'path')
+        .describe('p', 'write a path')
+        .alias('h', 'html')
+        .describe('h', 'use type of html')
+        .alias('s', 'single')
+        .describe('s', 'use type of single')
+        .argv;
 const cssControl = require('./lib/index');
 // 遍历目录
 let root = process.cwd();
-
+// 遍历类型
+let type = 'single';
 
 if(argv.h) {
-    
+    type = 'html';
+} else if (argv.s) {
+    type = 'single';
 }
 
 if(argv.p) {
@@ -14,5 +24,4 @@ if(argv.p) {
 }
 
 
-
-cssControl(root);
+cssControl(root, type);
